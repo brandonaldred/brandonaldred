3ad5b2c3dd9f36bb49fe1ed1d0639d2d0673d265
Brandon Aldred <brandonaldred@gmail.com>
2022-01-21 04:45:37 -0800

    Adding UX with hamburgr

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const menuIcon = document.querySelector('.menu');
+const mainContent = document.getElementById('main-content');
+
 menuIcon.addEventListener('click', ()=> {
     const navContainer = document.querySelector('.nav-container');
     if (navContainer.style.display === 'none') {
         navContainer.style.display = 'flex';
         menuIcon.className = 'menu selected';
+        mainContent.className = 'blur';
     } else {
         navContainer.style.display = 'none';
         menuIcon.className = 'menu';
+        mainContent.classList.remove('blur');
     }
 });
