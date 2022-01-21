635094516d5756a2269a8678c2dab67a74e28f42
Brandon Aldred <brandonaldred@gmail.com>
2022-01-21 04:52:11 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 const menuIcon = document.querySelector('.menu');
 const mainContent = document.getElementById('main-content');
 
+function menuClear() {
+    const navContainer = document.querySelector('.nav-container');
+    navContainer.style.display = 'none';
+    menuIcon.className = 'menu';
+    mainContent.classList.remove('blur');
+}
+
 menuIcon.addEventListener('click', ()=> {
     const navContainer = document.querySelector('.nav-container');
     if (navContainer.style.display === 'none') {
         navContainer.style.display = 'flex';
         menuIcon.className = 'menu selected';
         mainContent.className = 'blur';
+        mainContent.addEventListener('click', () => {
+            menuClear();
+        });
     } else {
-        navContainer.style.display = 'none';
-        menuIcon.className = 'menu';
-        mainContent.classList.remove('blur');
+        menuClear();
     }
 });
