8e9d56b49d12cdb5e0f7fb4020a4484ac88c26c8
Brandon Aldred <brandonaldred@gmail.com>
2022-01-22 04:28:46 -0800

    responsive menu

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const menuIcon = document.querySelector('.menu');
 const mainContent = document.getElementById('main-content');
 
 window.addEventListener("resize", function(){
+    mainContent.className = '';
     const navContainer = document.getElementById('nav-container');
     if (window.innerWidth >= 700) {
         navContainer.className = 'nav-show-wide';
-    } else { navContainer.className = 'hide'; }
+    } else {
+        navContainer.className = 'hide';
+    }
   });
 
 function menuClear() {
