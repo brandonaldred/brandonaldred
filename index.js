de14df20b14301027a7a437f35f0c442f09d3d70
Brandon Aldred <brandonaldred@gmail.com>
2022-02-10 06:30:39 -0800

    adding login functionality

--- index.js
+++ index.js
@@ -12,8 +12,26 @@ const Project = require('./models/project');
 mongoose.connect('mongodb://localhost/brandonaldred', { useNewUrlParser: true });
 
 const bodyParser = require('body-parser');
+const User = require('./models/user');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const bcrypt = require('bcrypt');
+
+const expressSession = require('express-session');
+app.use(expressSession({ secret: 'keyboard cat' }));
+
+const customMiddleWare = (req,res,next) => {
+    console.log('custom middleware called');
+    next();
+}
+
+global.loggedIn = false;
+app.use('*', (req, res, next) => {
+    loggedIn = req.session.userId;
+    next();
+})
+
 
 const months = [
     'January',
@@ -47,8 +65,13 @@ app.listen(3000, () => {
         res.render('index', { projects, months })
     });
 
-    app.get('/compose', (req,res) => {
-        res.render('writePost');
+    app.get('/compose/write', (req,res) => {
+        let login = false;
+        if (req.session.userId) {
+            res.render('writePost');
+        } else {
+            res.redirect('/users/login');
+        }
     });
 
     app.post('/compose/store',(req,res) => {
@@ -59,7 +82,39 @@ app.listen(3000, () => {
 
     app.get('/project/:id', async (req,res) => {
         const project = await Project.findById(req.params.id);
+        console.log(req.session);
         res.render('project', { project, months });
+    });
+
+    app.get('/users/register', async (req, res) => {
+        res.render('register');
+    });
+
+    app.post('/users/register', (req, res) => {
+        User.create(req.body, (error, user) => {
+            if (error) {return res.redirect('/users/register'); }
+            res.redirect('/');
+        })
+    });
+
+    app.get('/users/login', async (req, res) => {
+       res.render('login'); 
+    });
+
+    app.post('/users/login', (req, res) => {
+        const { username, password } = req.body;
+
+        User.findOne({ username: username}, (error, user) => {
+            if(user) {
+                bcrypt.compare(password, user.password, (error, same) => {
+                    if(same) {
+                        req.session.userId = user._id;
+                        res.redirect('/');
+                    }
+                    else { res.redirect('/users/login'); }
+                });
+            }
+        })
     });
     
 });
